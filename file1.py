836cb9361e3ade010035a31db3a66c9bc531588e
unknown <you@example.com97523236+birusolankar@users.noreply.github.com>
2024-01-08 16:24:13 +0530

    feature signup working

---
+++ file1.py
@@ -0,0 +1,22 @@
+# check whether given number is prime number or not
+
+number = int(input("enter the number : "))
+
+flag = True
+
+if number <= 1:
+    print(f"{number} is not prime number")
+elif number > 1:
+    # lets check for factors
+    for i in range(2, number):
+        if number % i ==0:
+            flag = False
+            break
+if flag:
+    print(f"{number} is prime number")
+else:
+    print(f"{number} is not prime number")
+
+
+
+print("hello world")
