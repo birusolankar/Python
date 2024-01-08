5a00c75afdf214bccf465d1c12b49e79bcd5170c
unknown <you@example.com97523236+birusolankar@users.noreply.github.com>
2024-01-08 16:49:06 +0530

    odd or even number code

---
+++ file2.py
@@ -0,0 +1,15 @@
+# check whether given number is odd or even
+
+number = int(input("enter the number : "))
+
+flag = True # assume given number is even
+
+if number % 2 == 0:
+    flag = True
+else:
+    flag = False
+
+if flag:
+    print(f"{number} is even number")
+else:
+    print(f"{number} is odd number")
